10c897e60c7831306539a590acff9177be9dc859
Erik Collinder <collinder.erik@gmail.com>
2017-03-13 22:43:23 +0100

    rake file that works pretty well

--- rakefile.rb
+++ rakefile.rb
@@ -1,16 +1,21 @@
-desc 'Serve Jekyll so its accessible to local devices'
-task :hello do
-  puts "I'm meant to be serving"
-  status = system("jekyll serve --host 0.0.0.0")
-  puts status ? "Success" : "Failed"
-end
+namespace :serve do
+  task :default => :local
+  
+  desc 'Serve site to devices in network'
+  task :local do
+    puts "I'm meant to be serving site to devices in network"
+    status = system("jekyll serve --host 0.0.0.0")
+    puts status ? "Success" : "Failed"
+  end
 
-desc 'Jekyll serving with staging configuration file'
-task :staging do
-  sh "JEKYLL_ENV=development jekyll serve --config=_config.yml,_config.staging.yml"
-end
+  desc 'Jekyll serving with staging configuration file'
+  task :staging do
+    sh "JEKYLL_ENV=development jekyll serve --config=_config.yml,_config.staging.yml"
+  end
 
-desc 'Jekyll serving with production configuration file'
-task :production do
-  sh "JEKYLL_ENV=production jekyll serve --config=_config.yml,_config.production.yml"
+  desc 'Jekyll serving with production configuration file'
+  task :production do
+    sh "JEKYLL_ENV=production jekyll serve --config=_config.yml,_config.production.yml"
+  end
+
 end
