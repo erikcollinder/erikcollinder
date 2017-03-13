278ebfe0f2b50c3302ef2cf2a3aa57e8ce5762a8
Erik Collinder <collinder.erik@gmail.com>
2017-03-13 11:45:31 +0100

    rake file for production and staging

--- rakefile.rb
+++ rakefile.rb
@@ -4,3 +4,13 @@ task :hello do
   status = system("jekyll serve --host 0.0.0.0")
   puts status ? "Success" : "Failed"
 end
+
+desc 'Jekyll serving with staging configuration file'
+task :staging do
+  sh "JEKYLL_ENV=development jekyll serve --config=_config.yml,_config.staging.yml"
+end
+
+desc 'Jekyll serving with production configuration file'
+task :production do
+  sh "JEKYLL_ENV=production jekyll serve --config=_config.yml,_config.production.yml"
+end
